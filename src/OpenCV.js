11f7f865a5c31598fa059e39a1612d6549fcc2cb
Taylor Downs <downs.taylor@gmail.com>
2018-03-21 12:41:48 +0000

    wrap in promise

--- src/OpenCV.js
+++ src/OpenCV.js
@@ -1,46 +1,34 @@
 import cv from 'opencv4nodejs';
 
 export function findInImage(waldo, scene) {
-  const needle = cv.imread(waldo);
-  cv.imwrite('tmp/img/1.png', needle);
+  return new Promise((resolve, reject) => {
 
-  const haystack = cv.imread(scene);
-  cv.imwrite('tmp/img/2.png', haystack);
+    const needle = cv.imread(waldo);
+    cv.imwrite('tmp/img/1.png', needle);
 
-  // Match template (the brightest locations indicate the highest match)
-  const matched = haystack.matchTemplate(needle, 5);
-  cv.imwrite('tmp/img/matched.png', matched);
+    const haystack = cv.imread(scene);
+    cv.imwrite('tmp/img/2.png', haystack);
 
-  // Use minMaxLoc to locate the highest value (or lower, depending of the type of matching method)
-  const minMax = matched.minMaxLoc();
-  const { maxLoc: { x, y } } = minMax;
+    // Match template (the brightest locations indicate the highest match)
+    const matched = haystack.matchTemplate(needle, 5);
+    cv.imwrite('tmp/img/matched.png', matched);
 
-  // Draw bounding rectangle
-  haystack.drawRectangle(
-    new cv.Rect(x, y, needle.cols, needle.rows),
-    new cv.Vec(0, 255, 0),
-    2,
-    cv.LINE_8
-  );
+    // Use minMaxLoc to locate the highest value (or lower, depending of the type of matching method)
+    const minMax = matched.minMaxLoc();
+    const { maxLoc: { x, y } } = minMax;
 
-  // Open result in new window
-  cv.imwrite('tmp/img/wheres_waldo.png', haystack);
-  // };
+    // Draw bounding rectangle
+    haystack.drawRectangle(
+      new cv.Rect(x, y, needle.cols, needle.rows),
+      new cv.Vec(0, 255, 0),
+      2,
+      cv.LINE_8
+    );
+    cv.imwrite('tmp/img/wheres_waldo.png', haystack);
 
-  // const method = cv.TM_SQDIFF;
-  // const output = cv.matchTemplate(templ, method, img);
-  // return new Promise( ( resolve, reject ) => {
-  //   const image = cv.readImage( bPath, ( err, matrix ) => err ? reject( err ) : resolve( matrix ) );
-  // })
-  // return new Promise( ( resolve, reject ) => {
-  //   const templ = cv.readImage( sPath, ( err, matrix ) => err ? reject( err ) : resolve( matrix ) );
-  // })
+    console.log(minMax.maxLoc);
+    resolve(minMax.maxLoc);
 
-  //
-  // cv.readImage(bPath, function(err, im) {
-  //   if (err) return console.error('error loading image');
-  //   var output = im.matchTemplate(bPath, 3);
-  //   var matches = output.templateMatches(0.80, 1.0, 5, false);
-  //   console.log(matches);
-  // })
+  })
+
 }
